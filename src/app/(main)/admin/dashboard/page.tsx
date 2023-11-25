11daa6b53622027942deafa14f106abedd9ab2be
matheusOliv23 <matheushenri1996@gmail.com>
2023-11-24 23:53:17 -0300

    feat: add bg color to Topbar

--- src/app/(main)/admin/dashboard/page.tsx
+++ src/app/(main)/admin/dashboard/page.tsx
@@ -1,9 +1,5 @@
 import AdminContainer from '@/components/Container/AdminContainer'
 
 export default function page() {
-  return (
-    <div className="bg-primary-700">
-      <AdminContainer>dashboard</AdminContainer>
-    </div>
-  )
+  return <AdminContainer>dashboard</AdminContainer>
 }
--- src/components/Topbar/index.tsx
+++ src/components/Topbar/index.tsx
@@ -3,7 +3,7 @@ import Search from '../shared/Search'
 
 export default function Topbar() {
   return (
-    <header className="w-full border-b border-b-primary-400 p-10 md:h-[146px]">
+    <header className="w-full border-b border-b-primary-400 bg-primary-700 p-10 md:h-[146px]">
       <Search className="w-full max-w-[580px]" />
     </header>
   )
