810100e1c0654c2f4249540823af1135fa4a2de6
matheusOliv23 <matheushenri1996@gmail.com>
2023-11-24 23:46:36 -0300

    feat: add admin page

---
+++ src/app/(main)/admin/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import AdminContainer from '@/components/Container/AdminContainer'
+
+export default function page() {
+  return (
+    <div className="bg-primary-700">
+      <AdminContainer>dashboard</AdminContainer>
+    </div>
+  )
+}
